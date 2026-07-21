1f7c8d0780810b2eff7e03a7965de47091e38e7c
Awais Tanveer <awaistanveer210@gmail.com>
2026-07-21 12:49:52 +0500

    Added home.jsx and renamed js files to jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
    <div className="flex justify-center min-h-screen py-2 text-5xl font-bold bg-green-600 text-amber-600 capitalize" >
     food delivery project
+    <p>
+      this is my food website
+    </p>
    </div>
   );
 }
